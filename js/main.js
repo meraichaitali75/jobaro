fd38df4e03b0deb80bb8b46be3b4c27191f3d575
meraichaitali75 <meraichaitali75@gmail.com>
2024-05-11 20:41:07 -0400

    all the pages and UI designs created

---
+++ js/main.js
@@ -0,0 +1,544 @@
+ AOS.init({
+ 	duration: 800,
+ 	easing: 'slide',
+ 	once: true
+ });
+
+jQuery(document).ready(function($) {
+
+	"use strict";
+
+	var slider = function() {
+		$('.nonloop-block-3').owlCarousel({
+	    center: false,
+	    items: 1,
+	    loop: true,
+	    smartSpeed: 700,
+			stagePadding: 15,
+	    margin: 20,
+	    autoplay: true,
+	    nav: true,
+			navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+	    responsive:{
+        600:{
+        	margin: 20,
+          items: 2
+        },
+        1000:{
+        	margin: 20,
+          items: 3
+        },
+        1200:{
+        	margin: 20,
+          items: 3
+        }
+	    }
+		});
+	};
+	slider();
+
+
+	var siteMenuClone = function() {
+
+		$('<div class="site-mobile-menu"></div>').prependTo('.site-wrap');
+
+		$('<div class="site-mobile-menu-header"></div>').prependTo('.site-mobile-menu');
+		$('<div class="site-mobile-menu-close "></div>').prependTo('.site-mobile-menu-header');
+		$('<div class="site-mobile-menu-logo"></div>').prependTo('.site-mobile-menu-header');
+
+		$('<div class="site-mobile-menu-body"></div>').appendTo('.site-mobile-menu');
+
+		
+
+		$('.js-logo-clone').clone().appendTo('.site-mobile-menu-logo');
+
+		$('<span class="ion-ios-close js-menu-toggle"></div>').prependTo('.site-mobile-menu-close');
+		
+
+		$('.js-clone-nav').each(function() {
+			var $this = $(this);
+			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+		});
+
+
+		setTimeout(function() {
+			
+			var counter = 0;
+      $('.site-mobile-menu .has-children').each(function(){
+        var $this = $(this);
+        
+        $this.prepend('<span class="arrow-collapse collapsed">');
+
+        $this.find('.arrow-collapse').attr({
+          'data-toggle' : 'collapse',
+          'data-target' : '#collapseItem' + counter,
+        });
+
+        $this.find('> ul').attr({
+          'class' : 'collapse',
+          'id' : 'collapseItem' + counter,
+        });
+
+        counter++;
+
+      });
+
+    }, 1000);
+
+		$('body').on('click', '.arrow-collapse', function(e) {
+      var $this = $(this);
+      if ( $this.closest('li').find('.collapse').hasClass('show') ) {
+        $this.removeClass('active');
+      } else {
+        $this.addClass('active');
+      }
+      e.preventDefault();  
+      
+    });
+
+		$(window).resize(function() {
+			var $this = $(this),
+				w = $this.width();
+
+			if ( w > 768 ) {
+				if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+			}
+		})
+
+		$('body').on('click', '.js-menu-toggle', function(e) {
+			var $this = $(this);
+			e.preventDefault();
+
+			if ( $('body').hasClass('offcanvas-menu') ) {
+				$('body').removeClass('offcanvas-menu');
+				$this.removeClass('active');
+			} else {
+				$('body').addClass('offcanvas-menu');
+				$this.addClass('active');
+			}
+		}) 
+
+		// click outisde offcanvas
+		$(document).mouseup(function(e) {
+	    var container = $(".site-mobile-menu");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+	    }
+		});
+	}; 
+	siteMenuClone();
+
+
+	var sitePlusMinus = function() {
+		$('.js-btn-minus').on('click', function(e){
+			e.preventDefault();
+			if ( $(this).closest('.input-group').find('.form-control').val() != 0  ) {
+				$(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) - 1);
+			} else {
+				$(this).closest('.input-group').find('.form-control').val(parseInt(0));
+			}
+		});
+		$('.js-btn-plus').on('click', function(e){
+			e.preventDefault();
+			$(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) + 1);
+		});
+	};
+	sitePlusMinus();
+
+
+	var siteSliderRange = function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+	};
+	siteSliderRange();
+
+
+	var siteMagnificPopup = function() {
+		$('.image-popup').magnificPopup({
+	    type: 'image',
+	    closeOnContentClick: true,
+	    closeBtnInside: false,
+	    fixedContentPos: true,
+	    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+	     gallery: {
+	      enabled: true,
+	      navigateByImgClick: true,
+	      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+	    },
+	    image: {
+	      verticalFit: true
+	    },
+	    zoom: {
+	      enabled: true,
+	      duration: 300 // don't foget to change the duration also in CSS
+	    }
+	  });
+
+	  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+	    disableOn: 700,
+	    type: 'iframe',
+	    mainClass: 'mfp-fade',
+	    removalDelay: 160,
+	    preloader: false,
+
+	    fixedContentPos: false
+	  });
+	};
+	siteMagnificPopup();
+
+	var searchShow = function() {
+		// alert();
+		var searchWrap = $('.search-wrap');
+		$('.js-search-open').on('click', function(e) {
+			e.preventDefault();
+			searchWrap.addClass('active');
+			setTimeout(function() {
+				searchWrap.find('.form-control').focus();
+			}, 300);
+		});
+		$('.js-search-close').on('click', function(e) {
+			e.preventDefault();
+			searchWrap.removeClass('active');
+		})
+	};
+	searchShow();
+
+});
+
+
+ /* home autoplay slider */
+
+ $(document).ready(function() {
+   var owl = $('.owl-carousel');
+   owl.owlCarousel({
+     items: 6,
+     loop: true,
+     margin: 10,
+     autoplay: true,
+     autoplayTimeout: 5000,
+     autoplayHoverPause: false
+   });
+ })
+
+
+
+ /* home process */
+
+ $(function(){
+
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+
+  	if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+  		$('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+  	}
+
+  	sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+  } else {
+  	
+  	sr.reveal('.js--fadeInLeft', {
+	    origin: 'left',
+	    distance: '300px',
+		  easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+	  sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+  }
+  
+  sr.reveal('.js--fadeInLeft', {
+	    origin: 'left',
+	    distance: '300px',
+		  easing: 'ease-in-out',
+	    duration: 800,
+	  });
+
+	  sr.reveal('.js--fadeInRight', {
+	    origin: 'right',
+	    distance: '300px',
+	    easing: 'ease-in-out',
+	    duration: 800,
+	  });
+});
+
+
+/* choose file */
+
+//I added event handler for the file upload control to access the files properties.
+document.addEventListener("DOMContentLoaded", init, false);
+
+//To save an array of attachments
+var AttachmentArray = [];
+
+//counter for attachment array
+var arrCounter = 0;
+
+//to make sure the error message for number of files will be shown only one time.
+var filesCounterAlertStatus = false;
+
+//un ordered list to keep attachments thumbnails
+var ul = document.createElement("ul");
+ul.className = "thumb-Images";
+ul.id = "imgList";
+
+function init() {
+  //add javascript handlers for the file upload event
+  document
+    .querySelector("#files")
+    .addEventListener("change", handleFileSelect, false);
+}
+
+//the handler for file upload event
+function handleFileSelect(e) {
+  //to make sure the user select file/files
+  if (!e.target.files) return;
+
+  //To obtaine a File reference
+  var files = e.target.files;
+
+  // Loop through the FileList and then to render image files as thumbnails.
+  for (var i = 0, f; (f = files[i]); i++) {
+    //instantiate a FileReader object to read its contents into memory
+    var fileReader = new FileReader();
+
+    // Closure to capture the file information and apply validation.
+    fileReader.onload = (function(readerEvt) {
+      return function(e) {
+        //Apply the validation rules for attachments upload
+        ApplyFileValidationRules(readerEvt);
+
+        //Render attachments thumbnails.
+        RenderThumbnail(e, readerEvt);
+
+        //Fill the array of attachment
+        FillAttachmentArray(e, readerEvt);
+      };
+    })(f);
+
+    // Read in the image file as a data URL.
+    // readAsDataURL: The result property will contain the file/blob's data encoded as a data URL.
+    // More info about Data URI scheme https://en.wikipedia.org/wiki/Data_URI_scheme
+    fileReader.readAsDataURL(f);
+  }
+  document
+    .getElementById("files")
+    .addEventListener("change", handleFileSelect, false);
+}
+
+//To remove attachment once user click on x button
+jQuery(function($) {
+  $("div").on("click", ".img-wrap .close", function() {
+    var id = $(this)
+      .closest(".img-wrap")
+      .find("img")
+      .data("id");
+
+    //to remove the deleted item from array
+    var elementPos = AttachmentArray.map(function(x) {
+      return x.FileName;
+    }).indexOf(id);
+    if (elementPos !== -1) {
+      AttachmentArray.splice(elementPos, 1);
+    }
+
+    //to remove image tag
+    $(this)
+      .parent()
+      .find("img")
+      .not()
+      .remove();
+
+    //to remove div tag that contain the image
+    $(this)
+      .parent()
+      .find("div")
+      .not()
+      .remove();
+
+    //to remove div tag that contain caption name
+    $(this)
+      .parent()
+      .parent()
+      .find("div")
+      .not()
+      .remove();
+
+    //to remove li tag
+    var lis = document.querySelectorAll("#imgList li");
+    for (var i = 0; (li = lis[i]); i++) {
+      if (li.innerHTML == "") {
+        li.parentNode.removeChild(li);
+      }
+    }
+  });
+});
+
+//Apply the validation rules for attachments upload
+function ApplyFileValidationRules(readerEvt) {
+  //To check file type according to upload conditions
+  if (CheckFileType(readerEvt.type) == false) {
+    alert(
+      "The file (" +
+        readerEvt.name +
+        ") does not match the upload conditions, You can only upload jpg/png/gif files"
+    );
+    e.preventDefault();
+    return;
+  }
+
+  //To check file Size according to upload conditions
+  if (CheckFileSize(readerEvt.size) == false) {
+    alert(
+      "The file (" +
+        readerEvt.name +
+        ") does not match the upload conditions, The maximum file size for uploads should not exceed 300 KB"
+    );
+    e.preventDefault();
+    return;
+  }
+
+  //To check files count according to upload conditions
+  if (CheckFilesCount(AttachmentArray) == false) {
+    if (!filesCounterAlertStatus) {
+      filesCounterAlertStatus = true;
+      alert(
+        "You have added more than 10 files. According to upload conditions you can upload 10 files maximum"
+      );
+    }
+    e.preventDefault();
+    return;
+  }
+}
+
+//To check file type according to upload conditions
+function CheckFileType(fileType) {
+  if (fileType == "image/jpeg") {
+    return true;
+  } else if (fileType == "image/png") {
+    return true;
+  } else if (fileType == "image/gif") {
+    return true;
+  } else {
+    return false;
+  }
+  return true;
+}
+
+//To check file Size according to upload conditions
+function CheckFileSize(fileSize) {
+  if (fileSize < 300000) {
+    return true;
+  } else {
+    return false;
+  }
+  return true;
+}
+
+//To check files count according to upload conditions
+function CheckFilesCount(AttachmentArray) {
+  //Since AttachmentArray.length return the next available index in the array,
+  //I have used the loop to get the real length
+  var len = 0;
+  for (var i = 0; i < AttachmentArray.length; i++) {
+    if (AttachmentArray[i] !== undefined) {
+      len++;
+    }
+  }
+  //To check the length does not exceed 10 files maximum
+  if (len > 9) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+//Render attachments thumbnails.
+function RenderThumbnail(e, readerEvt) {
+  var li = document.createElement("li");
+  ul.appendChild(li);
+  li.innerHTML = [
+    '<div class="img-wrap"> <span class="close">&times;</span>' +
+      '<img class="thumb" src="',
+    e.target.result,
+    '" title="',
+    escape(readerEvt.name),
+    '" data-id="',
+    readerEvt.name,
+    '"/>' + "</div>"
+  ].join("");
+
+  var div = document.createElement("div");
+  div.className = "FileNameCaptionStyle";
+  li.appendChild(div);
+  div.innerHTML = [readerEvt.name].join("");
+  document.getElementById("Filelist").insertBefore(ul, null);
+}
+
+//Fill the array of attachment
+function FillAttachmentArray(e, readerEvt) {
+  AttachmentArray[arrCounter] = {
+    AttachmentType: 1,
+    ObjectType: 1,
+    FileName: readerEvt.name,
+    FileDescription: "Attachment",
+    NoteText: "",
+    MimeType: readerEvt.type,
+    Content: e.target.result.split("base64,")[1],
+    FileSizeInBytes: readerEvt.size
+  };
+  arrCounter = arrCounter + 1;
+}
+
+
+
+// order count timer 
+
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),
+    x = setInterval(function() {    
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'IT'S MY BIRTHDAY!;
+      //}
+
+    }, second)
